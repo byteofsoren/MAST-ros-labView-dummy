3bafcef2e2e5ee08169132cb6336cc35d47db39b
byteofsoren <r00tr4t@gmail.com>
2018-06-28 09:12:09 +0000

    aoeu
    Signed-off-by: byteofsoren <r00tr4t@gmail.com>

--- scripts/main.py
+++ scripts/main.py
@@ -3,23 +3,30 @@
 #  Created by Magnus S0rensen for the MAST project
 #  byteofsoren@gmail.com   june 26 2018
 # ---------------------------------------------------
+# ROS - LabView comunitation dummy that emulate the RoboRIO.
 
 import rospy
 from std_msgs.msg import String
+from ros_labview_dummy import from_rio
+from ros_labviw import to_rio
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size = 10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10Hz
-    while not rospy.is_shutdown():
-        hello_str = "Hello word from the dummy and the time is {}".format(rospy.get_time())
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+# Settings:
+subcribe_from_tx2 = 'control_to_rio'
+publish_to_tx2 = 'read_rio'
 
-if __name__== '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        print("An error have ocured")
-        pass
+# Globals:
+message_from_tx2 = to_rio()
+message_to_tx2 = from_rio()
+# Create a publisher so data can be sent from the rio to the tx2
+pub_handle = rospy.Publisher(publish_to_tx2, data_class=from_rio)
+update_rate = rospy.Rate(100)
+# Create a subcriber so the it can read the data from the tx2
+rospy.init_node(subcribe_from_tx2, anonymous=True)
+set_speed=0
+set_steering=0
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
